764ca2c5ac9d017ff7c254f1e4b033cbfbc17683
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-12-02 00:35:55 -0300

    javaMailSender

--- envioEmail/src/main/java/service/EmailService.java
+++ envioEmail/src/main/java/service/EmailService.java
@@ -1,8 +1,14 @@
 package service;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import com.fsc.models.EmailMessage;
 
 @Service
 public class EmailService {
@@ -10,7 +16,22 @@ public class EmailService {
 	@Autowired
 	private JavaMailSender javaMailSender;
 	
-	public void enviar() {
+	public void enviar(EmailMessage emailMessage)throws MessagingException {
+		
+		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+		
+		helper.setFrom(emailMessage.getRemetente());
+		helper.setSubject(emailMessage.getAssunto());
+		helper.setText(emailMessage.getMessagem());
+		helper.setTo(emailMessage.getDestinatario()
+				.toArray(new String[emailMessage.getDestinatario().size()]));
+		
+		javaMailSender.send(mimeMessage);
+		
+		
+		
+		
 		
 	}
 
