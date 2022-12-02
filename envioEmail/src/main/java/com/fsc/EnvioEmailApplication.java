d6449e2ddda02c018b2f11106c9652ea89f4c42f
Fernando-Programador <suporte@ajeitandoseulado.com.br>
2022-12-01 23:55:12 -0300

    implementacao  mail.properties

---
+++ envioEmail/src/main/java/com/fsc/EnvioEmailApplication.java
@@ -0,0 +1,13 @@
+package com.fsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioEmailApplication.class, args);
+	}
+
+}
